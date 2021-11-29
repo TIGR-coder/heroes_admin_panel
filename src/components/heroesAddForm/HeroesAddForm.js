1b32d881b469b3d3d2a047eb290df90f05102630
Ilya Vakhromeev <vakhromeev_ilya@bk.ru>
2021-11-29 14:08:59 +0300

    before RTK Query

---
+++ src/components/heroesAddForm/HeroesAddForm.js
@@ -0,0 +1,114 @@
+import { v4 as uuidv4 } from 'uuid';
+import { useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { useHttp } from '../../hooks/http.hook';
+import { heroCreated } from '../heroesList/heroesSlice';
+import { selectAll } from '../heroesFilters/filtersSlice';
+import store from '../../store';
+// Задача для этого компонента:
+// Реализовать создание нового героя с введенными данными. Он должен попадать
+// в общее состояние и отображаться в списке + фильтроваться
+// Уникальный идентификатор персонажа можно сгенерировать через uiid
+// Усложненная задача:
+// Персонаж создается и в файле json при помощи метода POST
+// Дополнительно:
+// Элементы <option></option> желательно сформировать на базе
+// данных из фильтров
+
+const HeroesAddForm = () => {
+	const [name, setName] = useState('');
+	const [description, setDescription] = useState('');
+	const [element, setElement] = useState('');
+
+	const dispatch = useDispatch();
+	const { request } = useHttp();
+	const { filtersLoadingStatus } = useSelector(state => state.filters);
+	const filters = selectAll(store.getState());
+
+	console.log(filters);
+	const onCreate = (e) => {
+		e.preventDefault();
+
+		const hero = {
+			id: uuidv4(),
+			name,
+			description,
+			element
+		};
+
+		request(`http://localhost:3001/heroes`, "POST", JSON.stringify(hero))
+			.then(data => console.log(data, 'Created'))
+			.then(dispatch(heroCreated(hero)))
+			.catch(err => console.log(err));
+
+		setName('');
+		setDescription('');
+		setElement('');
+	}
+
+	const renderFilters = (filters, status) => {
+		if (status === "loading") {
+			return <option>Загрузка элементов</option>
+		} else if (status === "error") {
+			return <option>Ошибка загрузки</option>
+		}
+
+		if (filters && filters.length > 0) {
+			return filters.map(({ name, label }) => {
+				// Один из фильтров нам тут не нужен
+				// eslint-disable-next-line
+				if (name === 'all') return;
+
+				return <option key={name} value={name}>{label}</option>
+			})
+		}
+	}
+	const options = renderFilters(filters, filtersLoadingStatus);
+	return (
+		<form onSubmit={onCreate} className="border p-4 shadow-lg rounded">
+			<div className="mb-3">
+				<label htmlFor="name" className="form-label fs-4">Имя нового героя</label>
+				<input
+					required
+					type="text"
+					name="name"
+					className="form-control"
+					id="name"
+					value={name}
+					onChange={(e) => setName(e.target.value)}
+					placeholder="Как меня зовут?" />
+			</div>
+
+			<div className="mb-3">
+				<label htmlFor="text" className="form-label fs-4">Описание</label>
+				<textarea
+					required
+					name="text"
+					className="form-control"
+					id="text"
+					value={description}
+					onChange={(e) => setDescription(e.target.value)}
+					placeholder="Что я умею?"
+					style={{ "height": '130px' }} />
+			</div>
+
+			<div className="mb-3">
+				<label htmlFor="element" className="form-label">Выбрать элемент героя</label>
+				<select
+					required
+					className="form-select"
+					id="element"
+					name="element"
+					value={element}
+					onChange={(e) => setElement(e.target.value)}>
+					<option >Я владею элементом...</option>
+					{options}
+				</select>
+			</div>
+
+			<button type="submit" className="btn btn-primary">Создать</button>
+		</form>
+	)
+}
+
+export default HeroesAddForm;
---
+++ src/store/index.js
@@ -0,0 +1,19 @@
+import { configureStore } from '@reduxjs/toolkit';
+import filters from '../components/heroesFilters/filtersSlice';
+import heroes from '../components/heroesList/heroesSlice';
+
+
+const stringMiddleware = () => (next) => (action) => {
+	if (typeof (action) === 'string') {
+		return next({ type: action })
+	}
+	return next(action);
+}
+
+const store = configureStore({
+	reducer: { filters, heroes },
+	middleware: getDefaultMiddleware => getDefaultMiddleware().concat(stringMiddleware),
+	devTools: process.env.NODE_ENV !== "production",
+});
+
+export default store;
